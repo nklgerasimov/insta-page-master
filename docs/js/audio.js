7b8a1b4cce5f0e5e8ba5f6a229ab35a621055ddf
nklgerasimov <gniko@yandex.ru>
2022-12-16 15:55:35 +0300

    1

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
     const audio = $('#audioTrack')[0];
-    const moove = 0;
     const playBtn = $('#audio-play');
     const pauseBtn = $('#audio-pause');
     const audioCtrl = $('[data-type="audio-control"]');
@@ -9,21 +8,34 @@ $(document).ready(function(){
     initAudioPlayer();
 
     // audio.play();
+    audio.play();
+    setTimeout(initAudioPlayer, 800);
+
     function initAudioPlayer() {
-        playBtn.parent().show();
+        // playBtn.parent().show();
+        pauseBtn.parent().show();
         audio.loop = true;
 
-
         audioCtrl.click(function() {
-            if (audio.paused) {
-                audio.play();
-                pauseBtn.parent().show();
-                playBtn.parent().hide();
-            } else {
+            if (audio.played) {
+                console.log('!');
                 audio.pause();
-                playBtn.parent().show();
                 pauseBtn.parent().hide();
+                playBtn.parent().show();
+            } else {
+                audio.play();
+                playBtn.parent().hide();
+                pauseBtn.parent().show();
             }
+            // if (audio.paused) {
+            //     audio.play();
+            //     pauseBtn.parent().show();
+            //     playBtn.parent().hide();
+            // } else {
+            //     audio.pause();
+            //     playBtn.parent().show();
+            //     pauseBtn.parent().hide();
+            // }
         })  
     }
 });
